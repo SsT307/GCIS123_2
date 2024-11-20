766373164634001943ef62d288146cb86b3cabbd
SsT307 <shaik.alhajri22@gmail.com>
2024-11-20 13:49:06 +0400

    do not open

--- (Do not open)/Project/assignment3.py
+++ (Do not open)/Project/assignment3.py
@@ -1,4 +1,13 @@
 
-dict = [34,78,965,64]
-h=tuple(dict)
-print(h)
+
+def m(i):
+    i +=1
+    if i <= 1/3:
+        return 1/3
+    elif i == 0:
+        return 0
+    else:
+        i-=1
+        m((i-1)/(2*i+1))
+
+print(m(3))
